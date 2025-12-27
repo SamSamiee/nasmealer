7cc7960a6ffa1f3f5ab1cb44623a99013e1d3fa7
Sam Samiee <samsamiee00@gmail.com>
2025-12-27 12:22:57 +0100

    db bugs fixed

--- server/db.js
+++ server/db.js
@@ -5,8 +5,7 @@ const pool = new Pool({
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
-  // port: process.env.DB_PORT
-  port: 5433,
+  port: process.env.DB_PORT,
 });
 
 pool
