3288e7a44c1cbd725f2ad13ef070aeabff1f0ad1
Sam Samiee <samsamiee00@gmail.com>
2026-01-06 14:17:56 +0100

    add getAllFriends functionality

--- server/routes/friends.routes.js
+++ server/routes/friends.routes.js
@@ -6,9 +6,9 @@ const pool = require("../db.js");
 // in friendship table request id should always be smaller than the friend_id
 // ============================================================================
 
-function normalizeUsers(u1, u2) {
-   const u1 = String(u1);
-   const u2 = String(u2);
+function normalizeUsers(U1, U2) {
+   const u1 = String(U1);
+   const u2 = String(U2);
    const userId = u1 < u2 ? u1 : u2;
    const friendId = u1 < u2 ? u2 : u1;
    return { userId, friendId };
@@ -19,6 +19,35 @@ const {
 } = require("../middlewares/auth.middleware.js");
 
 //GET ALL FRIENDS OF A USER
+router.get("/", authenticate, async (req, res, next) => {
+   const userId = req.user.userId;
+
+   try {
+      const result = await pool.query(
+         `
+            SELECT u.name, u.id, f.updated_at FROM friendships f
+            JOIN users u ON 
+                (f.user_id = $1 AND f.friend_id = u.id) OR
+                (f.friend_id = $1 AND f.user_id = u.id)
+            WHERE
+                (f.user_id = $1 OR
+                f.friend_id = $1)
+                AND f.status = 'accepted'
+            ORDER BY f.updated_at DESC
+            `,
+         [userId]
+      );
+
+      const friends = [];
+      result.rows.forEach(({ name, id, updated_at }) =>
+         friends.push({ name, id, updated_at })
+      );
+
+      return res.status(200).json(friends);
+   } catch (err) {
+      next(err);
+   }
+});
 
 // ADD A FRIEND
 router.post("/", authenticate, async (req, res, next) => {
@@ -65,17 +94,27 @@ router.post("/", authenticate, async (req, res, next) => {
                error: "Friend request already sent",
             });
          } else if (existingStatus === "rejected") {
-            pool.query(
+            const result = await pool.query(
                `
-                UPDATE friendships 
-                SET stauts='pending' 
+                UPDATE friendships
+                SET 
+                    status='pending'
+                    updated_at = CURRENT_TIMESTAMP
                 WHERE user_id=$1 AND 
                 friend_id=$2
+                RETURNING id
                 `,
                [userId, friendId]
             );
-         }
+            const {
+               rows: [{ id }],
+            } = result;
 
+            return res
+               .status(200)
+               .json({ message: "request sent", id });
+         }
+      } else {
          const result = await pool.query(
             `
         INSERT INTO friendships (user_id, friend_id) 
@@ -84,12 +123,13 @@ router.post("/", authenticate, async (req, res, next) => {
         `,
             [userId, friendId]
          );
+
          const {
             rows: [{ id }],
          } = result;
 
          return res
-            .status(204)
+            .status(201)
             .json({ message: "request sent", id });
       }
    } catch (err) {
